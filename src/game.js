39b6f35cfef465f8d82d263c39c6c1dff3c81d34
cdsuddreth <cdsuddreth@gmail.com>
2021-06-25 19:49:09 -0400

    created index.html and game.js

---
+++ src/game.js
@@ -0,0 +1,16 @@
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+
+game.state.add('play', {
+    preload: function(){
+    
+    },
+    create: function(){
+
+    },
+    render: function(){
+
+    }
+
+});
+
+game.state.start('play');
